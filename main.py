e40093d29e7bb3b5489a1bc24f5f6f9fa27cf184
andblizz <41571847+andblizz@users.noreply.github.com>
2021-05-06 10:48:40 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import os
+
+URL = 'https://auto.ria.com/newauto/marka-jaguar/'
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0', 'Accept': '*/*'}
+HOST = 'https://auto.ria.com'
+FILE = 'cars.csv'
+
+def get_html(url, params=None):
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+
+
+def get_pages_count(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    pagination = soup.find_all('span', class_='mhide')
+    if pagination:
+        return int(pagination[-1].get_text())
+    else:
+        return 1
+
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('div', class_='proposition')
+    cars = []
+    for item in items:
+        uah_price = item.find('span', class_=('green', 'bold', 'size22', 'tooltip-price'))
+        if uah_price:
+            uah_price = uah_price.get_text()
+        else:
+            uah_price = 'Цена не указана'
+        cars.append({
+            'title': item.find('span', class_='link').get_text(strip=True),
+            'link': HOST + item.find('a', class_='proposition_link').get('href'),
+            'usd_price': uah_price,
+            'uah_price': item.find('span', class_='size16').get_text(),
+            'region': item.find('span', class_='item region').get_text(),
+
+        })
+    return cars
+
+def save_file(items, path):
+    with open(path, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['Марка', 'Ссылка', 'Цена в USD', 'Цена в UAH', 'Город'])
+        for item in items:
+            writer.writerow([item['title'], item['link'], item['usd_price'], item['uah_price'], item['region']])
+
+
+def parse():
+    URL = input('Введите URL: ')
+    URL = URL.strip()
+    html = get_html(URL)
+    if html.status_code == 200:
+        cars = []
+        pages_count = get_pages_count(html.text)
+        for page in range(1, pages_count + 1):
+            print(f'Парсинг страницы {page} из {pages_count}...')
+            html = get_html(URL, params={'page': page})
+            cars.extend(get_content(html.text))
+        save_file(cars, FILE)
+        print(f'Получено {len(cars)} автомобилей')
+        os.startfile(FILE)
+    else:
+        print('Error')
+
+
+parse()
